2d8c7e6067f86aba3901be1554a3c0ec80e18c0a
asakhar <a@sakhar.ru>
2022-02-19 18:32:55 +0300

    remove old && reconfig launch

--- errors.cpp
+++
@@ -1,9 +0,0 @@
-#include "errors.hpp"
-
-void printError(char const *errortext, int errorcode) {
-  std::cerr << "Error: " << errortext;
-  if (errorcode != 0)
-    std::cerr << ": " << strerror(errorcode);
-  std::cerr << "\n";
-}
-char const *strend(char const *str) { return str + strlen(str); }
