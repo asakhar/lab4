a9cbb1ef5e17d361efbc896d01b0e8959865b5d5
asakhar <a@sakhar.ru>
2022-02-19 19:30:05 +0300

    create_process move to fn

--- main.rs
+++ main.rs
@@ -36,8 +36,19 @@ fn usage() {
   process::exit(1);
 }
 
+fn create_process(processor_name: &String, args: &Vec<String>) -> process::Child {
+  match process::Command::new(processor_name)
+    .args(args)
+    .stdout(process::Stdio::piped())
+    .spawn()
+  {
+    Err(_why) => error_and_exit!("Failed to start the process."),
+    Ok(res) => res,
+  }
+}
+
 fn main() {
-  let processor_name = "target/debug/processor";
+  let processor_name = "target/debug/processor".to_string();
   let args: Vec<String> = env::args().collect();
   if args.len() < 4 {
     error_and_exit!("Invalid number of arguments.", true);
@@ -45,9 +56,10 @@ fn main() {
   if args[3].len() != 1 {
     error_and_exit!("Invalid argument value for character to count.", true);
   }
+  let character_to_count = args[3].to_string();
 
   let file_name = args[1].to_string();
-  let processors_quantity = match args[2].parse::<u64>() {
+  let mut processors_quantity = match args[2].parse::<u64>() {
     Err(_why) => error_and_exit!("Invalid argument value for number of processes.", true),
     Ok(res) => res,
   };
@@ -60,6 +72,7 @@ fn main() {
   }
   if processors_quantity > (file_size >> 1) {
     println!("Warning: Quantity of processes specified ({}) exceeds half of the amount of information in file ({}).\nThe actual number of processes will be reduced...", processors_quantity, file_size>>1);
+    processors_quantity = file_size >> 1;
   }
   let block_size = file_size / processors_quantity;
   let last_block_size = file_size - block_size * (processors_quantity - 1);
@@ -67,37 +80,26 @@ fn main() {
   let mut processes = Vec::new();
 
   for i in 0..processors_quantity - 1 {
-    processes.push(
-      match process::Command::new(processor_name)
-        .args([
-          file_name.clone(),
-          block_size.to_string(),
-          (block_size * i).to_string(),
-          args[3].to_string(),
-        ])
-        .stdout(process::Stdio::piped())
-        .spawn()
-      {
-        Err(_why) => error_and_exit!("Failed to start the process."),
-        Ok(res) => res,
-      },
-    );
+    processes.push(create_process(
+      &processor_name,
+      &vec![
+        file_name.clone(),
+        block_size.to_string(),
+        (block_size * i).to_string(),
+        character_to_count.clone(),
+      ],
+    ));
   }
-  processes.push(
-    match process::Command::new(processor_name)
-      .args([
-        file_name,
-        last_block_size.to_string(),
-        (block_size * (processors_quantity - 1)).to_string(),
-        args[3].to_string(),
-      ])
-      .stdout(process::Stdio::piped())
-      .spawn()
-    {
-      Err(_why) => error_and_exit!("Failed to start the process."),
-      Ok(res) => res,
-    },
-  );
+  let last_offset = block_size * (processors_quantity - 1);
+  processes.push(create_process(
+    &processor_name,
+    &vec![
+      file_name,
+      last_block_size.to_string(),
+      last_offset.to_string(),
+      character_to_count,
+    ],
+  ));
   let mut result: u64 = 0;
   for proc in processes {
     let stdout = match proc.wait_with_output() {
